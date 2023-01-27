8fec1e2305e89ec4a2b2ed3dba6aeaa54b68fae2
Zan Markan <zan@markan.me>
2023-01-27 11:10:31 +0000

    wip something

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,43 @@ terraform {
 }
 
 provider "aws" {
-
     region = "us-east-1"
-  
 }
 
+resource "aws_iam_role_policy" "my_role_policy" {
+    provider = "oidc_provider_TODO"
+    name = "test_policy"
+    role = aws_iam_role.test_role.id
+
+    policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+  
