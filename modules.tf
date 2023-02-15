5196d12e71d708b5a5d96e569669c1e7c3feff26
Zan Markan <zan.markan@gmail.com>
2023-02-15 16:02:37 +0000

    typo

--- modules.tf
+++ modules.tf
@@ -1,7 +1,7 @@
 # TODO: Add new modules below by using the template
 module "aws_oicd_demo_ecr_project" {
   source = "./projects/aws_oicd_demo_ecr"
-  circleci_project_id = "6fb58098-00af-454d-994f-67833560caf2"
+  circleci_project_id = "fbac1980-6440-416f-a89c-2e2ac2b450c3"
   circleci_oidc_provider_base_url = var.circleci_oidc_provider_base_url
   circleci_oidc_org_id = var.circleci_oidc_org_id
   circleci_oidc_provider_arn = aws_iam_openid_connect_provider.circleci_oidc_provider.arn
