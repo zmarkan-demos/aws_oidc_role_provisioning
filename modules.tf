f66fc61633e747d46dc928e14c51c41211b32247
Zan Markan <zan.markan@gmail.com>
2023-02-15 15:50:17 +0000

    change project id in demo module

--- modules.tf
+++ modules.tf
@@ -1,7 +1,7 @@
 # TODO: Add new modules below by using the template
 module "aws_oicd_demo_ecr_project" {
   source = "./projects/aws_oicd_demo_ecr"
-  circleci_project_id = "b181484f-e483-4028-97ba-6efaa8445c97"
+  circleci_project_id = "6fb58098-00af-454d-994f-67833560caf2"
   circleci_oidc_provider_base_url = var.circleci_oidc_provider_base_url
   circleci_oidc_org_id = var.circleci_oidc_org_id
   circleci_oidc_provider_arn = aws_iam_openid_connect_provider.circleci_oidc_provider.arn
