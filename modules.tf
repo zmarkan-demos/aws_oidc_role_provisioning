5b8e3ca4a5207e738c837fab6f0c3927a29b3428
Zan Markan <zan.markan@gmail.com>
2023-02-15 13:59:57 +0000

    showcase - add demo project

--- modules.tf
+++ modules.tf
@@ -1,4 +1,11 @@
 # TODO: Add new modules below by using the template
+module "aws_oicd_demo_ecr_project" {
+  source = "./projects/aws_oicd_demo_ecr"
+  circleci_project_id = "b181484f-e483-4028-97ba-6efaa8445c97"
+  circleci_oidc_provider_base_url = var.circleci_oidc_provider_base_url
+  circleci_oidc_org_id = var.circleci_oidc_org_id
+  circleci_oidc_provider_arn = aws_iam_openid_connect_provider.circleci_oidc_provider.arn
+}
 
 # module "YOUR_PROJECT_NAME" {
 #   source = "./projects/YOUR_PROJECT_NAME"
